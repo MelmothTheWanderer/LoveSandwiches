69a77bd1308ef4b6c3bfb666fe1986d532ee7607
MelmothTheWanderer <>
2023-06-26 16:40:23 +0000

    added a fucntion that will validate the input

--- run.py
+++ run.py
@@ -22,8 +22,34 @@ def get_sales_data():
     print('Please enter sales data from the last market')
     print('Data should be six numbers , seperated by a comma')
     print('Example: 1, 2 , 3, 4 ,5 ,6')
-    
+
     data_string =    input('Please input your data here: ')
     print(f'The data you submitted was {data_string}')
 
+    sales_data = data_string.split(",")
+    validate_data(sales_data)
+
+
+def validate_data(values):
+    """
+    This function convert the string values into integers, and if
+    can't be done , then it will return an error , and also , will check to 
+    see if the data is 6 digits
+    """
+    # print(values)
+    # use a try to see if there is 6 numbers
+    try:
+        if len(values) != 6:
+            raise ValueError (f"Exactly 6 figures required here , and you have provided {len(values)}")
+        
+    except ValueError as e : 
+        print(f"Invalid data: {e}, please try again.\n")
+
+
 get_sales_data()
+
+
+
+
+
+
