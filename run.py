25e4f20edaef22bafa0a7f3e1fab7f3eafb305a3
MelmothTheWanderer <>
2023-06-28 17:37:20 +0000

    added a function that will append new data to the google spreadsheet .

--- run.py
+++ run.py
@@ -16,11 +16,13 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
+
 def get_sales_data():
 
-'''Get sales figures from the user'''
 
-        while True:
+    '''Get sales figures from the user'''
+
+    while True:
         print('Please enter sales data from the last market')
         print('Data should be six numbers , seperated by a comma')
         print('Example: 1, 2 , 3, 4 ,5 ,6')
@@ -34,28 +36,41 @@ def get_sales_data():
             print("Data is valid")
             break
 
+    return sales_data
+
 
 def validate_data(values):
+
     """
     This function convert the string values into integers, and if
     can't be done , then it will return an error , and also , will check to 
     see if the data is 6 digits
     """
-    # print(values)
-    # use a try to see if there is 6 numbers
     try:
-        #This isn't actually converting anything , because it isn't assigned to a variable . It is merely checking for the purposes of handling the error . 
+
         [int(value) for value in values]
         if len(values) != 6:
             raise ValueError (f"Exactly 6 figures required here , and you have provided {len(values)}")
         
-    except ValueError as e : 
+    except ValueError as e: 
         print(f"Invalid data: {e}, please try again.\n")
         return False
     
     return True
 
-get_sales_data()
+def update_sales_worksheet(data):
+
+    """Updates the google spreadsheet with new data"""
+
+    print("This is updating the spread sheet with new data./n")
+
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfuly/n")
+
+data = get_sales_data()
+
+sales_data = [int{num} for num in data]
 
 
 
